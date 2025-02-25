443f53a9215c205b9125f7837d7394ca21176929
jasha169 <giojasha4@gmail.com>
2025-02-25 09:41:16 -0800

    second commit

---
+++ day04/homeworks/lesson.04.py
@@ -0,0 +1,12 @@
+print("my mother is" " sofo " )
+print("my father is" " giga ")
+print("my sister is" " tako ")
+print("my brother is" " nika ")
+
+
+
+
+print(5+5)
+print(10-4)
+print(20*30)
+print(100/50)
