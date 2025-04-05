ed99253da7d72278fc408ed3ab299b9e1e417602
jasha169 <giojasha4@gmail.com>
2025-04-05 10:33:02 -0700

    seven commit

---
+++ day16/classworks/lesson.16.py
@@ -0,0 +1,5 @@
+sum = 0
+for i in range(1, 21):  
+    sum += i  
+
+print("1-დან 20-მდე რიცხვების ჯამი:", sum)
---
+++ day16/homeworks/lesson.16.py
@@ -0,0 +1,32 @@
+total_sum = 0
+for i in range(20, 41):  
+    total_sum += i
+
+print("რიცხვების ჯამი 20-იდან 40-მდე:", total_sum)
+
+
+total_sum = 0
+for i in range(10, 51):  
+    total_sum += i
+
+print("რიცხვების ჯამი 10-იდან 50-მდე:", total_sum)
+
+
+total_sum = 0
+for i in range(20, 101):  
+    if i % 2 == 0:  
+        total_sum += i
+
+print("ლუწი რიცხვების ჯამი 20-იდან 100-მდე:", total_sum)
+
+
+
+
+i = 0
+while i <= 50:
+    if i % 2 == 0:  
+        print(i)
+    i += 1  
+
+
+
